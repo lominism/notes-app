36eb7246ed2225159beaf9838d5054642347b01d
Lom <lominism@gmail.com>
2025-07-08 15:43:23 +0700

    fixed sidebar a little

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       {" "}
       {/* Wrap the layout with ProtectedRoute */}
       <SidebarProvider>
-        <AppSidebar />
+        <AppSidebar className="z-20" />
         <SidebarInset>
           <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4 z-10">
             <SidebarTrigger className="-ml-1" />
